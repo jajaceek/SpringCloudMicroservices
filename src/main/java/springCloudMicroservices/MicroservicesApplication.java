5db767f44e1bb366d8b58a7a6ac3de8df6797e04
Jacek <jacek@g.pl>
2020-12-10 10:43:53 +0100

    dodane servisy

--- src/main/java/springCloudMicroservices/MicroservicesApplication.java
+++ src/main/java/springCloudMicroservices/MicroservicesApplication.java
@@ -1,4 +1,4 @@
-package springCloudMicroservices.microservices;
+package springCloudMicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ user-service/src/main/java/springCloudMircoservices/UserApi.java
@@ -0,0 +1,17 @@
+package springCloudMircoservices;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApi {
+
+    @Value("${app.user}")
+    private String appUser;
+
+    @GetMapping
+    public String info() {
+        return "info from UserApi";
+    }
+}
